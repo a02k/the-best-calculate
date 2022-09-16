bef6fce4a3890e57195a72a9e7c1d6098396d6ac
Angelina <koao2002@mail.ru>
2022-09-16 21:36:58 +0300

    доработан js код калькулятор

--- js/script.js
+++ js/script.js
@@ -4,63 +4,89 @@ let first = 0;
 let second = 0;
 let d = '';
 let firstCulc = true;
+let hasDot = false;
 
-function append(num){
+function append(num) {
+    if (num == '.'){
+
+        if(hasDot == true){
+            return;
+        }
+
+        if (result.value == ''){
+            num = '0.';
+        }
+
+        hasDot == true;
+    }
     result.value = result.value + num;
 }
 
 function sum() {
     d = 'sum';
     first = parseFloat(result.value);
-    history.value = result.value + '+ ';
+    history.value = result.value + '+';
     result.value = '';
 }
 
 function equals() {
     second = parseFloat(result.value);
     history.value = history.value + second;
-    if (d == 'sum'){
+    if (d == '+'){
         result.value = first + second;
     }
 
-    if (d == 'divide'){
+    if (d == '/'){
         result.value = first / second;
     }
 
-    if (d == 'multiply'){
+    if (d == '*'){
         result.value = first * second;
     }
 
-    if (d == 'minus'){
+    if (d == '-'){
         result.value = first - second;
     }
 }
 
-function divide(){
-    d = 'divide';
-    first = parseFloat(result.value);
-    history.value = result.value + '/ ';
-    result.value = '';
-}
-
-function multiply(){
-    d = 'multiply';
-    first = parseFloat(result.value);
-    history.value = result.value + '*';
-    result.value = '';
-}
-
-function minus(){
-    d = 'minus';
-    first = parseFloat(result.value);
-    history.value = result.value + '-';
-    result.value = '';
-}
-
-function clear() {
+function clearCe() {
     d = '';
     history.value = '';
     result.value = '';
     first = 0;
     second = 0;
+    hasDot = false;
 }
+
+function clearC() {
+    result.value = '';
+    second = 0;
+    hasDot = false;
+}
+
+function actionCalculate(sign) {
+    d = sign;
+    first = parseFloat(result.value);
+    history.value = result.value + sign;
+    result.value = '';
+    hasDot = false;
+}
+
+/*
+var a = 5;
+test(10,a);
+test(15);
+
+function test(b,c){
+    console.log(b);
+    console.log(c);
+}
+
+test(20);
+
+function sing(b,c){
+    let a = 'b + c;
+    return a;
+}
+
+console.log(sing(10,15));*/
